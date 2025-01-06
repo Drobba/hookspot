d394627dfaa5820458bed33b2e375819b1b3c654
Andreas Nelefelt <andreas.nelefelt@elvenite.com>
2025-01-06 01:36:18 +0100

    Adding tailwind and angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, MatButtonModule], // Lägg till HeaderComponent här
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'], // Ändrat till styleUrls
 })
 export class AppComponent {
   title = 'hookspot';
 
   hello() {
     const hi = 'hello';
-
     const bye = 'bye';
   }
 }
