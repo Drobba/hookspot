bf4c318f345736bed8745db903cf118207e5c55d
Andreas Nelefelt <andreas.nelefelt@elvenite.com>
2025-06-24 11:15:24 +0200

    style: fixed mobile styling for status filter on mobile

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -151,7 +151,7 @@ export class MapComponent implements OnInit {
     }
 
     if (this.filterSettings.showOnlyMyCatches) {
-      parts.push('My catches only');
+      parts.push('My catches');
     }
     
     return parts.join(' • ');
